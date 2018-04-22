455e1fbc41a802bc8ad06c41309bbce346a16776
Redrield <redrield@gmail.com>
2018-04-21 22:16:18 -0400

    Works good yos

--- saturnlint/src/main/java/frc/team4069/saturn/lint/IncorrectSpellingLint.kt
+++ saturnlint/src/main/java/frc/team4069/saturn/lint/IncorrectSpellingLint.kt
@@ -1,22 +1,24 @@
 package frc.team4069.saturn.lint
 
-import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.LocalQuickFixBase
 import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.search.searches.ReferencesSearch
 import org.apache.log4j.Level
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
-import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
 
 class IncorrectSpellingLint : AbstractKotlinInspection() {
 
     val spellingMap = mapOf(
-            "meter" to "metre"
+            "meter" to "metre",
+            "organize" to "organise"
     )
 
     private val logger = Logger.getInstance(this::class.java)
@@ -33,83 +35,28 @@ class IncorrectSpellingLint : AbstractKotlinInspection() {
         return "frc.team4069.saturn.lint"
     }
 
-    override fun checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
-        if (file !is KtFile) {
-            return null
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = object : PsiElementVisitor() {
+        override fun visitElement(element: PsiElement?) {
+            if (element is KtNamedDeclaration) {
+                checkNode(element, holder)
+            } else if (element is LeafPsiElement && element !is PsiWhiteSpace) {
+                for ((wrong, correct) in spellingMap) {
+                    if (element.text.contains(wrong)) {
+                        holder.registerProblem(element, "Non-conforming spelling of $correct", SpellingQuickFix(wrong))
+                    }
+                }
+            }
         }
-
-        val problems = mutableListOf<ProblemDescriptor>()
-        file.findChildrenByClass(KtNamedDeclaration::class.java).forEach {
-            logger.debug("We got one! $it")
-        }
-
-
-
-//        for (childNode in file.allChildren) {
-//            when(childNode) {
-//                is KtNamedDeclaration -> {
-//                    checkNode(childNode, manager, isOnTheFly, problems)
-//
-//                    when (childNode) {
-//                        is KtNamedFunction -> {
-//                            for (node in childNode.valueParameters) {
-//                                checkNode(node, manager, isOnTheFly, problems)
-//                            }
-//                            childNode.children.forEach {
-//                                logger.debug("Function child $it")
-//                            }
-//
-//                            for(node in childNode.children.find { it is KtBlockExpression }?.children
-//                                    ?.filterIsInstance<KtNamedDeclaration>() ?: listOf()) {
-//                                checkNode(node, manager, isOnTheFly, problems)
-//                            }
-//                        }
-//                        is KtClass -> {
-//                            for(node in childNode.primaryConstructorParameters) {
-//                                checkNode(node, manager, isOnTheFly, problems)
-//                            }
-//
-//                            for(cons in childNode.secondaryConstructors) {
-//                                for(node in cons.valueParameters) {
-//                                    checkNode(node, manager, isOnTheFly, problems)
-//                                }
-//                            }
-//
-//                            if(childNode.hasBody()) {
-//                                val body = childNode.getBody()!!
-//
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                is KtConstructor<*> -> {
-//                    for(node in childNode.getValueParameters()) {
-//                        checkNode(node, manager, isOnTheFly, problems)
-//                    }
-//                }
-//            }
-//        }
-
-//        traverseNode(file, manager, isOnTheFly, problems)
-
-        return problems.toTypedArray()
     }
 
-    private fun checkNode(node: KtNamedDeclaration, manager: InspectionManager, isOnTheFly: Boolean, problems: MutableList<ProblemDescriptor>) {
-        val name = node.name!!
+    private fun checkNode(node: KtNamedDeclaration, holder: ProblemsHolder) {
+        val name = node.name ?: return
 
         for ((wrong, correct) in spellingMap) {
-            val nameNode = node.nameIdentifier!!
+            val nameNode = node.nameIdentifier ?: return
 
             if (name.contains(wrong, true)) {
-                problems.add(manager.createProblemDescriptor(
-                        nameNode,
-                        "Non-conforming spelling of $correct",
-                        SpellingQuickFix(wrong),
-                        ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                        isOnTheFly
-                ))
+                holder.registerProblem(nameNode, "Non-conforming spelling of $correct", SpellingQuickFix(wrong))
             }
         }
     }
@@ -118,6 +65,7 @@ class IncorrectSpellingLint : AbstractKotlinInspection() {
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
             val correct = spellingMap[wrong]!!
             val element = descriptor.psiElement
+
 
             val parent = element.parent
             when (parent) {
@@ -129,9 +77,15 @@ class IncorrectSpellingLint : AbstractKotlinInspection() {
                     }
 
                     parent.setName(newName)
+                    return
                 }
             }
 
+            if (element is LeafPsiElement) {
+                val newText = element.text.laxReplace(wrong, correct)
+                element.replaceWithText(newText)
+                return
+            }
         }
     }
 }
