a631a077ebf74af8754e8c7e24c4cb675fc00ab9
Redrield <redrield@gmail.com>
2018-10-01 17:14:04 -0400

    Post build time 2018-10-01
    * Switch SaturnRobot to extend Iterative, Timed giving issues
    
    * Added BrownoutWatchdog to provide a way to notify user code of
    critical voltage levels

--- src/main/kotlin/frc/team4069/saturn/lib/SaturnRobot.kt
+++ src/main/kotlin/frc/team4069/saturn/lib/SaturnRobot.kt
@@ -1,6 +1,17 @@
 package frc.team4069.saturn.lib
 
-import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.wpilibj.IterativeRobot
+import edu.wpi.first.wpilibj.command.Scheduler
+import frc.team4069.saturn.lib.util.BrownoutWatchdog
 
-abstract class SaturnRobot : TimedRobot()
+abstract class SaturnRobot : IterativeRobot() {
+    val brownoutWatchdog = BrownoutWatchdog(::notifyBrownout)
+
+    override fun robotPeriodic() {
+        Scheduler.getInstance().run()
+//        brownoutWatchdog.feed()
+    }
+
+    open fun notifyBrownout() {}
+}
 
