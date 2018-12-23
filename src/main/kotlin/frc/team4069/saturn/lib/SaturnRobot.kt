46fe7e2c2dee6216ce2f323d9a473c94a4175f83
Redrield <redrield@gmail.com>
2018-12-22 23:18:38 -0500

    Acquire Phoenix vendor specifier and uncomment CTRE stuff

--- src/main/kotlin/frc/team4069/saturn/lib/SaturnRobot.kt
+++ src/main/kotlin/frc/team4069/saturn/lib/SaturnRobot.kt
@@ -1,14 +1,23 @@
 package frc.team4069.saturn.lib
 
+import edu.wpi.first.hal.FRCNetComm
+import edu.wpi.first.hal.HAL
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.command.Scheduler
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import frc.team4069.saturn.lib.hid.SaturnHID
 import frc.team4069.saturn.lib.util.BrownoutWatchdog
 
+//const val kLanguageKotlin = 6
+
 abstract class SaturnRobot : TimedRobot() {
     private val brownoutWatchdog = BrownoutWatchdog(::notifyBrownout)
     private val controls = mutableListOf<SaturnHID<*>>()
+
+    // If we actually want unique reporting for kotlin, use this
+//    override fun robotInit() {
+//        HAL.report(FRCNetComm.tResourceType.kResourceType_Language, kLanguageKotlin)
+//    }
 
     override fun robotPeriodic() {
         Scheduler.getInstance().run()
--- src/main/kotlin/frc/team4069/saturn/lib/sensors/AHRS.kt
+++ src/main/kotlin/frc/team4069/saturn/lib/sensors/AHRS.kt
@@ -1,5 +1,6 @@
 package frc.team4069.saturn.lib.sensors
 
+import com.ctre.phoenix.sensors.PigeonIMU
 import frc.team4069.saturn.lib.mathematics.units.Rotation2d
 import frc.team4069.saturn.lib.mathematics.units.degree
 
@@ -15,15 +16,15 @@ abstract class AHRSSensor : IAHRSSensor {
     override val correctedAngle:Rotation2d get() = yaw + angleOffset
 }
 
-//class Pigeon(val pigeon: PigeonIMU) : AHRSSensor() {
-//
-//    override val yaw: Rotation2d
-//        get() = pigeon.fusedHeading.degree
-//
-//    override fun reset() {
-//        pigeon.setYaw(0.0, 0)
-//    }
-//
-//}
-//
-//fun PigeonIMU.toSaturn() = Pigeon(this)
+class Pigeon(val pigeon: PigeonIMU) : AHRSSensor() {
+
+    override val yaw: Rotation2d
+        get() = pigeon.fusedHeading.degree
+
+    override fun reset() {
+        pigeon.setYaw(0.0, 0)
+    }
+
+}
+
+fun PigeonIMU.toSaturn() = Pigeon(this)
