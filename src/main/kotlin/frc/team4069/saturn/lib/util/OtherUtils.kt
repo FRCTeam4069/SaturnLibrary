8dd8ad2904d6bc956f71c439567768c1c12f3db8
Redrield <redrield@gmail.com>
2019-02-02 12:43:26 -0500

    Fix bug in deadband

--- src/main/kotlin/frc/team4069/saturn/lib/util/OtherUtils.kt
+++ src/main/kotlin/frc/team4069/saturn/lib/util/OtherUtils.kt
@@ -1,9 +1,10 @@
 package frc.team4069.saturn.lib.util
 
+import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.min
 
 fun Double.safeRangeTo(endInclusive: Double) = min(this, endInclusive)..max(this, endInclusive)
 
 // Eagerly evaluated deadband
-fun Double.deadband(threshold: Double) = if(this < threshold) 0.0 else this
+fun Double.deadband(threshold: Double) = if(this.absoluteValue < threshold) 0.0 else this
