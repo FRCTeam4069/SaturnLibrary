ac0823070b1e31ca3aa66303e81a0c49d034d840
Redrield <redrield@gmail.com>
2019-05-21 10:28:15 -0400

    Add sensorPhase to CTRE encoder

--- src/main/kotlin/frc/team4069/saturn/lib/motor/ctre/SaturnCTREEncoder.kt
+++ src/main/kotlin/frc/team4069/saturn/lib/motor/ctre/SaturnCTREEncoder.kt
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.IMotorController
 import frc.team4069.saturn.lib.mathematics.units.SIUnit
 import frc.team4069.saturn.lib.mathematics.units.nativeunits.*
 import frc.team4069.saturn.lib.motor.AbstractSaturnEncoder
+import kotlin.properties.Delegates
 
 class SaturnCTREEncoder<T: SIUnit<T>>(
         val motorController: IMotorController,
@@ -15,6 +16,10 @@ class SaturnCTREEncoder<T: SIUnit<T>>(
 
     override val rawPosition: NativeUnit
         get() = motorController.getSelectedSensorPosition(pidIdx).STU
+
+    var encoderPhase by Delegates.observable(false) { _, _, newValue ->
+        motorController.setSensorPhase(newValue)
+    }
 
     override fun resetPosition(newPosition: Double) {
         motorController.setSelectedSensorPosition(newPosition.toInt(), pidIdx, 0)
