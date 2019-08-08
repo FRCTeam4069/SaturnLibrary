8957eba816d5253819d7fe74e5f35c416c363684
Redrield <redrield@gmail.com>
2019-08-08 02:13:09 -0400

    Add project license header to source files

--- core/src/main/kotlin/frc/team4069/saturn/lib/mathematics/units/SIUnit.kt
+++ core/src/main/kotlin/frc/team4069/saturn/lib/mathematics/units/SIUnit.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Lo-Ellen Robotics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package frc.team4069.saturn.lib.mathematics.units
 
 import frc.team4069.saturn.lib.mathematics.epsilonEquals
@@ -17,13 +33,15 @@ import kotlin.math.absoluteValue
  * NOTE: Comparisons using == and != should NOT be used with this wrapper, as it disregards the unit type and only compares the value stored within.
  * <, <=, >, and >= however, are typechecked and safe to use
  */
-inline class SIUnit<T: Key>(val value: Double) : Comparable<SIUnit<T>> {
+inline class SIUnit<T : Key>(val value: Double) : Comparable<SIUnit<T>> {
     operator fun unaryMinus() = SIUnit<T>(-value)
 
     operator fun plus(other: SIUnit<T>) =
             SIUnit<T>(value + other.value)
+
     operator fun minus(other: SIUnit<T>) =
             SIUnit<T>(value - other.value)
+
     operator fun times(scalar: Double) = SIUnit<T>(value * scalar)
     operator fun div(scalar: Double) = SIUnit<T>(value / scalar)
 
@@ -44,9 +62,10 @@ inline class SIUnit<T: Key>(val value: Double) : Comparable<SIUnit<T>> {
     val absoluteValue get() = SIUnit<T>(value.absoluteValue)
 }
 
-operator fun <T: Key, U: Key> SIUnit<T>.div(other: SIUnit<U>) =
+operator fun <T : Key, U : Key> SIUnit<T>.div(other: SIUnit<U>) =
         SIUnit<Fraction<T, U>>(value / other.value)
-operator fun <T: Key, U: Key> SIUnit<T>.times(other: SIUnit<U>) =
+
+operator fun <T : Key, U : Key> SIUnit<T>.times(other: SIUnit<U>) =
         SIUnit<Mult<T, U>>(value * other.value)
 
-operator fun <T: Key> Number.times(other: SIUnit<T>) = other * toDouble()
+operator fun <T : Key> Number.times(other: SIUnit<T>) = other * toDouble()
